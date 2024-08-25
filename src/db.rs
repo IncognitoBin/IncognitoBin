985485765109ceafa524e3056441299af3685947
X-SP33D <annas.lahrouri@gmail.com>
2024-08-26 00:46:01 +0100

    Just For Test If Git Is Working

--- src/db.rs
+++ src/db.rs
@@ -13,7 +13,7 @@ pub fn init_db(db_name: &str) -> Result<Connection> {
     )?;
     let mut stmt = conn.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?1")?;
     let mut rows = stmt.query(["chunks"])?;
-    if(!rows.next()?.is_some()){
+    if (!rows.next()?.is_some()) {
         // insert the chunks
     }
     Ok(conn)
