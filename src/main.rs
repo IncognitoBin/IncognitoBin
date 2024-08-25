4ec1b4c9b4a90b6b1b368cbd4a9a910683a03942
X-SP33D <annas.lahrouri@gmail.com>
2024-08-25 23:17:21 +0100

    convert from lazy_static to once_cell

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
+use once_cell::sync::Lazy;
 use std::sync::Mutex;
-use lazy_static::lazy_static;
-
-static SPLIT_SIZE: u16 = 30000;
+use std::collections::VecDeque;
+const SPLIT_SIZE: u16 = 30000;
 struct Chunk {
     id: u16,
     start: u128,
     end: u128,
     size: u8,
 }
-lazy_static! {
-    static ref CHUNKS: Mutex<Vec<Chunk>> = Mutex::new(Vec::new());
+struct DbOperation {
+    chunk: Chunk,
+    operation: u8,
 }
+static DB_UPDATE_QUEUE: Lazy<Mutex<VecDeque<DbOperation>>> = Lazy::new(|| Mutex::new(VecDeque::new()));
+static CHUNKS: Lazy<Mutex<Vec<Chunk>>> = Lazy::new(|| Mutex::new(Vec::new()));
 fn upgrade_chunk(chunk_to_index: usize) {
-    let mut chunks = CHUNKS.lock().unwrap();
-    if(chunks[chunk_to_index].start==chunks[chunk_to_index].end){
-        chunks[chunk_to_index].size += 1;
-        let start: u128 = 62_u128.pow(chunks[chunk_to_index].size as u32 -1);
-        let end: u128 = 62_u128.pow(chunks[chunk_to_index].size as u32) - 1;
+    let mut chunks = CHUNKS.lock().expect("Failed to lock CHUNKS mutex");
+    let chunk = &mut chunks[chunk_to_index];
+    if chunk.start == chunk.end {
+        chunk.size += 1;
+        let start: u128 = 62_u128.pow(chunk.size as u32 - 1);
+        let end: u128 = 62_u128.pow(chunk.size as u32) - 1;
         let chunk_size: u128 = (end - start) / SPLIT_SIZE as u128;
-        chunks[chunk_to_index].start = start + chunk_size * chunks[chunk_to_index].size as u128;
-        chunks[chunk_to_index].end = if chunks[chunk_to_index].size as u16 == SPLIT_SIZE - 1 {
+        chunk.start = start + chunk_size * chunk.size as u128;
+        chunk.end = if chunk.size as u16 == SPLIT_SIZE - 1 {
             end
         } else {
-            start + chunk_size * chunks[chunk_to_index].size as u128 + chunk_size - 1
+            start + chunk_size * chunk.size as u128 + chunk_size - 1
         };
     }
+    let mut db_update_queue = DB_UPDATE_QUEUE.lock().expect("Failed to lock DB QUEUE mutex");
+    db_update_queue.push_back(DbOperation {
+        operation:1,
+        chunk: Chunk {
+            id :chunk.id,
+            start :chunk.start,
+            end :chunk.end,
+            size :chunk.size
+        }
+    })
+
 }
+
 fn init(start_id_size: u8) {
-    let mut chunks = CHUNKS.lock().unwrap();
+    let mut chunks = CHUNKS.lock().expect("Failed to lock CHUNKS mutex");
     let start: u128 = 62_u128.pow(start_id_size as u32 - 1);
     let end: u128 = 62_u128.pow(start_id_size as u32) - 1;
     let chunk_size: u128 = (end - start) / SPLIT_SIZE as u128;
@@ -44,16 +60,7 @@ fn init(start_id_size: u8) {
         });
     }
 }
-// 13_537_086_546_263_552    (9 characters)
-// 218_340_105_584_896       (8 characters)
-// 56_800_235_584            (7 characters)
-// 916_132_832               (6 characters)
-// 14_776_336                (5 characters)
-// 238_328                   (4 characters)
-// 3_844                     (3 characters)
 fn main() {
-    // Minimum 3
     init(5);
     upgrade_chunk(0);
-
 }
