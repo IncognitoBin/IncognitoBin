7c38045b826304e3ef433e485332c1730ac2f6e5
X-SP33D <annas.lahrouri@gmail.com>
2024-08-30 21:31:06 +0100

    Better IDs Handling

--- src/chunks.rs
+++ src/chunks.rs
@@ -5,42 +5,57 @@ use serde::{Deserialize, Serialize};
 use crate::SPLIT_SIZE;
 use std::io::{BufWriter, ErrorKind, Result, Write};
 use std::io::BufReader;
+use std::collections::VecDeque;
 
-#[derive(Serialize, Deserialize, Clone,Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Chunk {
     pub(crate) id: u16,
     pub(crate) start: u128,
     pub(crate) end: u128,
     pub(crate) size: u8,
 }
-pub(crate) static CHUNKS: Lazy<RwLock<Vec<Chunk>>> = Lazy::new(|| {
-    RwLock::new(vec![])
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct ChunkManager {
+    pub(crate) chunks: Vec<Chunk>,
+    pub(crate) expired: VecDeque<UsedID>,
+}
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct UsedID {
+    pub(crate) id: u128,
+    pub(crate) available: i64,
+}
+pub(crate) static MANAGER: Lazy<RwLock<ChunkManager>> = Lazy::new(|| {
+    RwLock::new(ChunkManager {
+        chunks: Vec::new(),
+        expired: VecDeque::new(),
+    })
 });
+
 pub fn load() -> Result<()> {
     let file = match File::open("data.json") {
-        Ok(file) => { file },
+        Ok(file) => { file }
         Err(ref e) if e.kind() == ErrorKind::NotFound => {
             let mut file = File::create("data.json")?;
             init(5);
-            let chunks = CHUNKS.read().unwrap().clone();
-            serde_json::to_writer(&mut file, &chunks)?;
-            return Ok(())
+            let manager = MANAGER.read().unwrap().clone();
+            serde_json::to_writer(&mut file, &manager)?;
+            return Ok(());
         }
-        Err(e) => return Err(e.into()), // Return other errors
+        Err(e) => return Err(e.into()),
     };
     let reader = BufReader::new(file);
-    let chunks: Vec<Chunk> = serde_json::from_reader(reader)?;
-    let mut chunks_lock = CHUNKS.write().unwrap();
+    let chunks: ChunkManager = serde_json::from_reader(reader)?;
+    let mut chunks_lock = MANAGER.write().unwrap();
     *chunks_lock = chunks;
     Ok(())
 }
 pub fn init(start_id_size: u8) {
-    let mut chunks = CHUNKS.write().expect("Failed to lock CHUNKS mutex");
+    let mut manager = MANAGER.write().expect("Failed to lock CHUNKS mutex");
     let start: u128 = 62_u128.pow(start_id_size as u32 - 1);
     let end: u128 = 62_u128.pow(start_id_size as u32) - 1;
     let chunk_size: u128 = (end - start) / SPLIT_SIZE as u128;
     for i in 0..SPLIT_SIZE {
-        chunks.push(Chunk {
+        manager.chunks.push(Chunk {
             id: i,
             start: start + chunk_size * i as u128,
             end: if i == SPLIT_SIZE - 1 {
@@ -53,8 +68,8 @@ pub fn init(start_id_size: u8) {
     }
 }
 pub fn upgrade_chunk(chunk_to_index: usize) {
-    let mut chunks = CHUNKS.write().unwrap();
-    let chunk = &mut chunks[chunk_to_index];
+    let mut manager = MANAGER.write().unwrap();
+    let chunk = &mut manager.chunks[chunk_to_index];
     if chunk.start == chunk.start && chunk.size < 21 {
         chunk.size += 1;
         let start: u128 = 62_u128.pow(chunk.size as u32 - 1);
@@ -71,8 +86,8 @@ pub fn upgrade_chunk(chunk_to_index: usize) {
 pub fn store_chunks() -> Result<()> {
     let file = File::create("data.json")?;
     let mut writer = BufWriter::new(file);
-    let chunks = CHUNKS.read().unwrap();
-    serde_json::to_writer(&mut writer, &*chunks)?;
+    let mut manager = MANAGER.read().unwrap();
+    serde_json::to_writer(&mut writer, &*manager)?;
     writer.flush()?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::thread;
+use std::time::Duration;
+
 mod chunks;
 const SPLIT_SIZE: u16 = 30000;
 fn main() {
+
     match chunks::load() {
         Ok(()) => println!("Data loaded successfully!"),
         Err(e) => eprintln!("Failed to load data: {}", e),
@@ -10,4 +14,12 @@ fn main() {
         Ok(()) => println!("Saved"),
         Err(e) => eprintln!("Failed to store data: {}", e),
     }
+
+    let handle = thread::spawn(|| {
+        loop {
+            chunks::store_chunks().expect("Can't Store The File!");
+            thread::sleep(Duration::from_secs(15));
+        }
+    });
+    handle.join().unwrap();
 }
