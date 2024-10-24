9a7f575cfb755207422979f8604bff07972c0c55
X-SP33D <annas.lahrouri@gmail.com>
2024-10-24 04:09:03 +0100

    Client: Add Toast Support

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,20 +1,21 @@
-import './App.css'
-import { ThemeProvider } from "@/components/theme-provider"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CreatePaste from './pages/Create';
+import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CreatePaste from "./pages/Create";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
-
+  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <Router>
-      <Routes>
-        <Route path="/Create" element={<CreatePaste />} />
-      </Routes>
-    </Router>
-    
+      <Router>
+        <Routes>
+          <Route path="/Create" element={<CreatePaste />} />
+        </Routes>
+      </Router>
+      <Toaster />
     </ThemeProvider>
   );
 }
 
-export default App
+export default App;
