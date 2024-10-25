be083e91ec562517cacdec3dc67ad25d6f5d725e
X-SP33D <annas.lahrouri@gmail.com>
2024-10-25 01:44:19 +0100

    Client: Read Paste Page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreatePaste from "./pages/Create";
+import ReadPaste from "./pages/Read";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/Create" element={<CreatePaste />} />
+          <Route path="/:id" element={<ReadPaste />} />
+          <Route path="/:id/:key" element={<ReadPaste />} />
+          <Route path="/:id/:key/:iv" element={<ReadPaste />} />
         </Routes>
       </Router>
       <Toaster />
