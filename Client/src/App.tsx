db2dee2439ef689b0c60278c33da6a5c6ea150a6
X-SP33D <annas.lahrouri@gmail.com>
2024-10-27 03:18:14 +0100

    Client: Include the Header and Set Routes For Auth, User Pastes and Default Page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,17 +4,25 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreatePaste from "./pages/Create";
 import ReadPaste from "./pages/Read";
 import { Toaster } from "./components/ui/toaster";
+import AuthPage from "./pages/Auth";
+import MyPastes from "./pages/MyPastes";
+import Header from "./layouts/Header";
 
 function App() {
-  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
+      <Header />
         <Routes>
+          <Route path="/" element={<CreatePaste />} />
           <Route path="/Create" element={<CreatePaste />} />
           <Route path="/:id" element={<ReadPaste />} />
           <Route path="/:id/:key" element={<ReadPaste />} />
+          
           <Route path="/:id/:key/:iv" element={<ReadPaste />} />
+          <Route path="/Auth" element={<AuthPage />} />
+          <Route path="/Me" element={<MyPastes />} />
+          
         </Routes>
       </Router>
       <Toaster />
